4052f907d3ef52fa5db4cff46540b07fbd73a74a
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-13 11:06:58 -0700

    added files

---
+++ script.js
@@ -0,0 +1,31 @@
+/* (".title-1").click(function() {
+  $(".text-1").slideToggle();
+  /* $(".dollar-1").toggle();
+});
+$(".title-2").click(function() {
+  $(".text-2").slideToggle();
+  /* $(".dollar-2").toggle();
+});
+$(".title-3").click(function() {
+  $(".text-3").slideToggle();
+  /*  $(".dollar-3").toggle();
+});
+$("#professional-reasons").click(function() {
+  $("#email-button").html("Professional Email Request");
+});
+$("#personal-reasons").click(function() {
+  $("#email-button").html("Personal Email Request");
+});
+$("#email-body").keypress(function() {
+  $("#email-button").removeAttr("disabled");
+});
+$("#Are-you-Human").click(function() {
+  $("#Are-you-Human").removeClass("is-invalid");
+});
+$("#email-button").click(function() {
+  window.open(
+    "mailto:christia.corrales001@mymdc.net?subject=Hello&body=" +
+      $("#email-body").val(),
+    "_blank"
+  );
+}); */
