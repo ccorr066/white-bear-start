32df6fa1a47d038f90e83e62f8988817395ceb43
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-13 17:14:14 -0700

    added error banner

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $("#save-imagery").click(function () {
    $("#overlay-success").toggleClass("d-flex d-none");
 });
+
+$("#create-error").click(function () {
+   $("#overlay-danger").toggleClass("d-flex d-none");
+});
