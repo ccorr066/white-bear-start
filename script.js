c889508b4c385f1c0bc5ea97051c4bd265f3ea16
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-22 12:11:27 -0700

    before revision

--- script.js
+++ script.js
@@ -1,56 +1,63 @@
-$(document).ready(function () {
-   $("#save-imagery").click(function () {
-      $("#overlay-success").toggleClass("d-flex d-none");
-   });
+$("#save-imagery").click(function () {
+   $("#overlay-success").toggleClass("d-flex d-none");
+});
 
-   $("#create-error").click(function () {
-      $("#overlay-danger").toggleClass("d-flex d-none");
-   });
+$("#create-error").click(function () {
+   $("#overlay-danger").toggleClass("d-flex d-none");
+});
 
-   $("#delete").hide(),
-      $("#check-box").click(function () {
-         $("#delete").toggle("#delete");
-      });
-   $("#drop-assign, #sign-up-text").hide(),
-      $("#sign-up").click(function () {
-         $("#drop-assign, #sign-up-text").slideDown(
-            "#drop-assign, #sign-up-text"
-         );
-      });
-   $("#sign-up").click(function () {
-      $("#sign-up").hide();
-   });
-   $("#lets-go").click(function () {
-      let emailLength = $("#email-identity").val().length;
-      let passwordLength = $("#password-required").val().length;
+$("#delete").hide();
 
-      if (emailLength === 0) {
-         $("#email-identity").addClass("is-invalid");
-         $("#error-email").html("Please enter your email address.");
-      } else {
-         $("#email-identity").removeClass("is-invalid");
-         $("#email-identity").addClass("is-valid");
-         $("#error-email").html("");
-      }
-      if (passwordLength === 0) {
-         $("#password-required").addClass("is-invalid");
-         $("#invalid-characters").html(
-            "Your password must be at least 9 characters."
-         );
-      } else {
-         $("#password-required").removeClass("is-invalid");
-         $("#password-required").addClass("is-valid");
-         $("#invalid-characters").html("");
-      }
-   });
+$("#check-box").click(function () {
+   $("#delete").toggle("#delete");
+});
 
-   $("#textBox").keyup(function () {
-      let textAreaCount = $("#textBox").val().length;
-      $("#count").html(`${textAreaCount}/240`);
-      if (textAreaCount > 240) {
-         $("#count").addClass("text-danger");
-      } else {
-         $("#count").removeClass("text-danger");
-      }
-   });
+$("#drop-assign, #sign-up-text").hide();
+
+$("#sign-up").click(function () {
+   $("#drop-assign, #sign-up-text").slideDown("#drop-assign, #sign-up-text");
+});
+
+$("#sign-up").click(function () {
+   $("#sign-up").hide();
+});
+
+$("#lets-go").click(function () {
+   var emailInput = $("#email-identity").val(); // .val gets the value of the user entered
+   console.log(emailInput);
+
+   var emailLength = emailInput.length; // .length gets the length of a string
+   console.log(emailLength);
+
+   // let emailLength = $("#email-identity").val().length;
+   let passwordLength = $("#password-required").val().length;
+
+   if (emailLength === 0) {
+      $("#email-identity").addClass("is-invalid");
+      $("#error-email").html("Please enter your email address.");
+   } else {
+      $("#email-identity").removeClass("is-invalid");
+      $("#email-identity").addClass("is-valid");
+      $("#error-email").html("");
+   }
+   if (passwordLength === 0) {
+      $("#password-required").addClass("is-invalid");
+      $("#invalid-characters").html(
+         "Your password must be at least 9 characters."
+      );
+   } else {
+      $("#password-required").removeClass("is-invalid");
+      $("#password-required").addClass("is-valid");
+      $("#invalid-characters").html("");
+   }
+});
+
+$("#textBox").keyup(function () {
+   let textAreaCount = $("#textBox").val().length;
+   $("#count").html(`${textAreaCount}/240`);
+   if (textAreaCount > 240) {
+      $("#count").addClass("text-danger");
+   } else {
+      $("#count").removeClass("text-danger");
+   }
 });
