12ffb1c4f1950665c83f77ba205116712a48eb27
ccorr066@fiu.edu <punchcode@Punchcodes-MacBook-Pro.local>
2020-05-26 09:57:53 -0700

    fix the bug on if else condition for password

--- script.js
+++ script.js
@@ -27,13 +27,13 @@ $("#lets-go").click(function () {
    console.log(emailInput);
 
    var emailLength = emailInput.length; // .length gets the length of a string
-   console.log(emailLength);
+   // console.log(emailLength);
 
    var passwordInput = $("#password-required").val(); // . val gets the value from the user on the password box
    console.log(passwordInput);
 
    var passwordLength = passwordInput.length; // .length grabs the amount of characters  of string
-   console.log(passwordLength);
+   // console.log(passwordLength);
 
    if (emailLength === 0) {
       // make sure if emailLength equals 0, then the if statement will run test under its condition
@@ -45,12 +45,11 @@ $("#lets-go").click(function () {
       $("#email-identity").addClass("is-valid"); // .addClass will take is-valid and let the email box work properly
       $("#error-email").html(""); // the error message will not appear.
    }
-   if (passwordInput === 0) {
+
+   if (passwordLength === 0) {
       // make sure if passwordInput equals 0 the if statement will run test under its condition
       $("#password-required").addClass("is-invalid"); // addClass will take is-valid and let the password box work properly
-      $("#invalid-characters").html(
-         "Your password must be at least 9 characters."
-      ); // error message will appear
+      $("#invalid-characters").html("Please enter password."); // error message will appear
    } else if (passwordInput.length < 9) {
       // make sure the password charcter has to be over 9. if not the else if will run test
 
